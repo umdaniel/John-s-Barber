a7ab1e30cdfa606c47b9fcce318f521a95d93f62
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-07-02 15:48:26 -0400

    Navbar and sidebar implemented

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeroSection = () => {
+    return (
+        <HeroContainer id="home">
+            <HeroBg>
+                <img src="" alt="haircut" />
+            </HeroBg>
+        </HeroContainer>
+    );
+};
+
+export default HeroSection;
