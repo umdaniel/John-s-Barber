e90d13f4f723c3e566508f77834f9904d910924c
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-07-04 22:05:09 -0400

    Added HeroSection

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { HeroContainer, HeroBg, HeroContent,
+HeroBtnWrapper, HeroH1, HeroP, ArrowForward,
+ArrowRight } from './HeroElements';
+import { Button } from '../ButtonElement';
+import Main from '../../images/barber.jpg';
 
 const HeroSection = () => {
+    
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
     return (
         <HeroContainer id="home">
             <HeroBg>
-                <img src="" alt="haircut" />
+                <img src={Main} alt="haircut"
+                width= "100%"
+                height= "100%"
+                object-fit="cover" />
             </HeroBg>
+            <HeroContent>
+                <HeroH1>A Barber Shop You Can Trust</HeroH1>
+                <HeroP>Reserve your seat</HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup"
+                    onMouseEnter={onHover}
+                    onMouseLeave={onHover}
+                    primary="true"
+                    dark="true">
+                        Make an appointment {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     );
 };
