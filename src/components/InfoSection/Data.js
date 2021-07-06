0cbb16b185a7800f490f7e038daf15812567a926
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-07-05 20:06:39 -0400

    Added info section, about us, services, and footer

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,6 +1,6 @@
 
 export const homeObjOne = {
-    id: 'about',
+    id: 'reserve',
     lightBg: false,
     lightText: true,
     lightTextDesc: true,
@@ -9,9 +9,43 @@ export const homeObjOne = {
     description: 'Reserve a seat and indulge yourself in a haircutting experience you will remember.',
     buttonLabel: 'Reserve Now',
     imgStart: false,
-    img: require('../../images/haircut.jpg').default,
+    img: require('../../images/haircut.png').default,
     alt: 'haircut',
     dark: true,
     primary: true,
     darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'about',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'About Us',
+    headline: 'A barber shop should be family',
+    description: "Here at John's Barber we treat our customers like family. We want you to feel comfortable and at home.",
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/community.png').default,
+    alt: 'about',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join Us',
+    headline: 'Let us remember you',
+    description: 'Sign up for an account and have easier access to reservations.',
+    buttonLabel: 'Sign Up',
+    imgStart: false,
+    img: require('../../images/login.png').default,
+    alt: 'sign',
+    dark: false,
+    primary: false,
+    darkText: true
 }
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -8,7 +8,7 @@ TextWrapper, Column1, Heading, BtnWrap, Subtitle } from './InfoElements';
 const InfoSection = ({lightBg, id, imgStart,
     topLine, lightText, headline,
     darkText, description, buttonLabel,
-    img, alt
+    img, alt, primary, dark, dark2
 }) => {
     return (
         <>
@@ -21,7 +21,18 @@ const InfoSection = ({lightBg, id, imgStart,
                             <Heading lightText={lightText}>{headline}</Heading>
                             <Subtitle darkText={darkText}>{description}</Subtitle>
                             <BtnWrap>
-                                <Button to="home">{buttonLabel}</Button>
+                                <Button 
+                                to="home"
+                                smooth={true}
+                                duration={500}
+                                spy={true}
+                                exact="true"
+                                offset={-80}
+                                primary={primary ? 1 : 0}
+                                dark={dark ? 1 : 0}
+                                dark2={dark2 ? 1 : 0} >
+                                    {buttonLabel}
+                                </Button>
                             </BtnWrap>
                         </TextWrapper>
                         </Column1>
