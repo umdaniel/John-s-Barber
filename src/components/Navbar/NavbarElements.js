b88c89f3322dd3edd9ee7c8195c6e360f6bd03ef
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-07-06 13:41:02 -0400

    Footer and signup created

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -4,7 +4,7 @@ import { Link as LinkR } from 'react-router-dom';
 import { Link as LinkS } from 'react-scroll';
 
 export const Nav = styled.nav`
-background: #000;
+background: ${({ scrollNav }) => (scrollNav ? '#000' : '#000')};
 height: 80px
 margin-top: -80px;
 display: flex;
